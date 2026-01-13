e5daae383166d106fa871ba5633187c081c94725
arile <dltmddnr0208@gmail.com>
2026-01-13 12:39:23 +0900

    #4 - Spring security filter chain에 custom filter 등록: SecurityConfig
    cors는 일단 기본값으로 세팅하고,
    모든 request를 허용하도록 세팅함.
    JWT 인증 필터를 등록함.

--- stock-system/src/main/java/arile/toy/stocksystem/config/SecurityConfig.java
+++ stock-system/src/main/java/arile/toy/stocksystem/config/SecurityConfig.java
@@ -1,17 +1,32 @@
 package arile.toy.stocksystem.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
 
+    @Autowired private JwtAuthenticationFilter jwtAuthenticationFilter;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+                .cors(Customizer.withDefaults())
+                .authorizeHttpRequests(requests -> requests.anyRequest().permitAll())
+                .sessionManagement(
+                        (session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .csrf(CsrfConfigurer::disable)
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .httpBasic(HttpBasicConfigurer::disable)
                 .logout(logout -> logout.logoutSuccessUrl("/"))
                 .build();
     }
