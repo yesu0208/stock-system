3fb8298f13626ed3f374a18f807ef844ee13c44b
arile <dltmddnr0208@gmail.com>
2026-01-13 11:16:59 +0900

    #3 - UserEntity 구현: implements UserDetails
    username, password, role, createdDateTime을 담는 UserEntity 구현함.
    Role enum을 만들어서 각 유저에 USER, ADMIN role 부여함.

---
+++ stock-system/src/main/java/arile/toy/stocksystem/domain/entity/UserEntity.java
@@ -0,0 +1,89 @@
+package arile.toy.stocksystem.domain.entity;
+
+import arile.toy.stocksystem.domain.user.Role;
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.time.ZonedDateTime;
+import java.util.Collection;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+@Entity
+@Table(name = "users")
+public class UserEntity implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column
+    @Enumerated(value = EnumType.STRING)
+    private Role role;
+
+    @Column(nullable = false)
+    private ZonedDateTime createdDateTime;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public @Nullable String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+
+    public static UserEntity of(String username, String password) {
+        var UserEntity = new UserEntity();
+        UserEntity.setUsername(username);
+        UserEntity.setPassword(password);
+        UserEntity.setRole(Role.USER);
+        return UserEntity;
+    }
+
+
+    @PrePersist
+    private void prePersist() {
+        this.createdDateTime = ZonedDateTime.now();
+    }
+}
