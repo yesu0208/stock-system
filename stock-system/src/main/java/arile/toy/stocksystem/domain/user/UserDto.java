b01b2b49250e5cc1b44d633800937148ac1caa47
arile <dltmddnr0208@gmail.com>
2026-01-13 11:18:38 +0900

    #3 - UserDto
    client에게 내려줄 전용 Dto 생성함.
    (userId, username만 전달)

---
+++ stock-system/src/main/java/arile/toy/stocksystem/domain/user/UserDto.java
@@ -0,0 +1,9 @@
+package arile.toy.stocksystem.domain.user;
+
+import arile.toy.stocksystem.domain.entity.UserEntity;
+
+public record UserDto(Long userId, String username) {
+    public static UserDto from(UserEntity userEntity) {
+        return new UserDto(userEntity.getUserId(), userEntity.getUsername());
+    }
+}
