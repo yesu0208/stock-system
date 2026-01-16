9b2630064f095f49a8371a5ad16f6aaba3e01ba2
arile <dltmddnr0208@gmail.com>
2026-01-16 21:05:37 +0900

    #10 - optimize imports

--- stock-system/src/main/java/arile/toy/stocksystem/config/SecurityConfig.java
+++ stock-system/src/main/java/arile/toy/stocksystem/config/SecurityConfig.java
@@ -3,7 +3,6 @@ package arile.toy.stocksystem.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
