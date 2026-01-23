b76b2423db73078c2f6996fac0a7c42382abb9e3
arile <dltmddnr0208@gmail.com>
2026-01-23 19:51:31 +0900

    #11 - Handler: diapatcher -> handler -> publisher
    diapatcher에서 온 data 중 필요한 부분만 추출하여 data -> Message -> Event type로 변환 후 publisher에게 전달함.
    publisher는 Redis에 event를 publish하는 역할을 함.

---
+++ stock-system/src/main/java/arile/toy/stocksystem/stockserver/external/stock/handler/StockSummaryTickMessageHandler.java
@@ -0,0 +1,51 @@
+package arile.toy.stocksystem.stockserver.external.stock.handler;
+
+import arile.toy.stocksystem.stockserver.external.stock.event.StockSummaryTickEvent;
+import arile.toy.stocksystem.stockserver.external.stock.event.publisher.RedisStockSummaryEventPublisher;
+import arile.toy.stocksystem.stockserver.external.stock.message.StockSummaryTickMessage;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class StockSummaryTickMessageHandler {
+
+    private final RedisStockSummaryEventPublisher redisStockSummaryEventPublisher;
+
+    public void handle(String message) {
+
+        String[] parts = message.split("\\|", 4);
+
+//      String encrypted = parts[0];
+        String trId = parts[1];
+        int count = Integer.parseInt(parts[2]); // 한 메시지에 여러 개의 데이터 들어있을 수 있다
+        String payload = parts[3];
+
+//      복호화(생략)
+//      if ("1".equals(encrypted)) {
+//          payload = aes256.decrypt(payload, key, iv);
+//      }
+
+        String[] fields = payload.split("\\^");
+        int offset;
+
+        int fieldSize = 46;
+
+        for (int i = 0; i < count; i++) {
+            offset = i * fieldSize;
+
+            String stockCode = fields[offset];
+
+            StockSummaryTickMessage stockSummaryTickMessage = new StockSummaryTickMessage(
+                    stockCode,
+                    Integer.parseInt(fields[offset + 2]),
+                    Integer.parseInt(fields[offset + 4])
+            );
+
+            redisStockSummaryEventPublisher.publish(
+                    StockSummaryTickEvent.fromMessage(stockSummaryTickMessage));
+        }
+    }
+}
