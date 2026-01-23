b8adc627c9262085d824511fa24eeaba28c843a8
arile <dltmddnr0208@gmail.com>
2026-01-24 00:26:04 +0900

    #13 - Security: package 구조 변경
    BFF server에서 이루어지는 인증 시스템.

---
+++ stock-system/src/main/java/arile/toy/stocksystem/bffserver/security/config/JwtExceptionFilter.java
@@ -0,0 +1,39 @@
+package arile.toy.stocksystem.bffserver.security.config;
+
+import arile.toy.stocksystem.bffserver.exception.dto.ErrorResponse;
+import io.jsonwebtoken.JwtException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import tools.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+
+@Component
+public class JwtExceptionFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        try {
+            filterChain.doFilter(request, response);
+        } catch (JwtException exception) {
+            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.setCharacterEncoding("UTF-8");
+
+            var errorResponse = new ErrorResponse(HttpStatus.UNAUTHORIZED, exception.getMessage());
+
+            // json으로 직렬화하여 전달
+            ObjectMapper objectMapper = new ObjectMapper();
+            String responseJson = objectMapper.writeValueAsString(errorResponse);
+            response.getWriter().write(responseJson);
+        }
+    }
+}
