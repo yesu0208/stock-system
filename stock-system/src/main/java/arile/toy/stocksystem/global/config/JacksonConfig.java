a69c5e70f8f6ccc6161ede502719a4e467a96076
arile <dltmddnr0208@gmail.com>
2026-01-23 20:00:40 +0900

    #11 - Config: global config 따로 패키징
    전체 서버(stock server, bff server)를 위한 config 파일을 global로 모음.
    이는 개발의 편의성을 위한 것으로, 추후 서버 분리 시 config 파일도 분리할 수 있음.

---
+++ stock-system/src/main/java/arile/toy/stocksystem/global/config/JacksonConfig.java
@@ -0,0 +1,16 @@
+package arile.toy.stocksystem.global.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JacksonConfig {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return JsonMapper.builder()
+                .build();
+    }
+}
