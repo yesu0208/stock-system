08cb0a4a2cb5a107cab8f8bd04a610a14a24b9cd
arile <dltmddnr0208@gmail.com>
2026-01-24 00:30:21 +0900

    #13 - StompConfig: package 구조 변경
    BFF server - client 간에 STOMP를 사용하여 message 실시간으로 주고 받음.

---
+++ stock-system/src/main/java/arile/toy/stocksystem/bffserver/stomp/config/StompConfig.java
@@ -0,0 +1,29 @@
+package arile.toy.stocksystem.bffserver.stomp.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@EnableWebSocketMessageBroker
+@Configuration
+public class StompConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws-stock")
+                .setAllowedOriginPatterns("*")
+                .withSockJS();
+        registry.addEndpoint("/ws-order")
+                .setAllowedOriginPatterns("*")
+                .withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/sub");
+        registry.setApplicationDestinationPrefixes("/app");
+        registry.setUserDestinationPrefix("/user");
+    }
+}
