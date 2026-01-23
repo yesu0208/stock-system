496bd49ec32c7177272d982c70a0797e0b6c584d
arile <dltmddnr0208@gmail.com>
2026-01-23 17:34:17 +0900

    #11 - Manager, Listener: 외부 api connection/subscribe
    Listener에서는 외부 api와 ws connection을 맺고,
    특정 종목에 대해 subscribe를 하는 기능을 제공한다.
    subscribe하는 종목의 대상은 stockserverrk 다루는 종목에 따라 달라지며, 추후 application.yaml로 이동하여 관리할 예정임.

---
+++ stock-system/src/main/java/arile/toy/stocksystem/stockserver/external/stock/manager/ExternalStockConnectionManager.java
@@ -0,0 +1,30 @@
+package arile.toy.stocksystem.stockserver.external.stock.manager;
+
+import arile.toy.stocksystem.stockserver.external.stock.approvalkey.ApprovalKeyService;
+import arile.toy.stocksystem.stockserver.external.stock.listener.ExternalStockListener;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+
+@Component
+@RequiredArgsConstructor
+public class ExternalStockConnectionManager {
+
+    private final List<String> stockCodes =
+            List.of("247540"); // application.yaml로 이동
+
+    private final ApprovalKeyService approvalKeyService;
+    private final ExternalStockListener externalStockListener;
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void connectAndSubscribe() {
+        String approvalKey = approvalKeyService.issueApprovalKey();
+
+        externalStockListener.connect(approvalKey);
+        stockCodes.forEach(externalStockListener::subscribe);
+    }
+}
