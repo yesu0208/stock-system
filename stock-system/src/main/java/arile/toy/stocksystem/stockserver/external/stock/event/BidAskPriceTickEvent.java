9d1a099d6f647f5390002c2247a40dd217b666fd
arile <dltmddnr0208@gmail.com>
2026-01-29 17:50:37 +0900

    #14 - Event 변경: stockcode만 포함
    bff server에서 했던 작업과 동일하게,
    event는 stockcode만 포함하도록 변경함.

--- stock-system/src/main/java/arile/toy/stocksystem/stockserver/external/stock/event/BidAskPriceTickEvent.java
+++ stock-system/src/main/java/arile/toy/stocksystem/stockserver/external/stock/event/BidAskPriceTickEvent.java
@@ -1,26 +1,13 @@
 package arile.toy.stocksystem.stockserver.external.stock.event;
 
 import arile.toy.stocksystem.stockserver.external.stock.message.BidAskPriceTickMessage;
-import arile.toy.stocksystem.stockserver.external.stock.message.TickMessageType;
-
-import java.util.List;
 
 public record BidAskPriceTickEvent(
-        TickMessageType tickMessageType,
-        String stockCode,
-        List<PriceLevel> asks,
-        List<PriceLevel> bids,
-        Integer totalAskNum,
-        Integer totalBidNum
+        String stockCode
 ) {
     public static BidAskPriceTickEvent fromMessage(BidAskPriceTickMessage message) {
         return new BidAskPriceTickEvent(
-                message.tickMessageType(),
-                message.stockCode(),
-                message.asks() == null ? List.of() : List.copyOf(message.asks()),
-                message.bids() == null ? List.of() : List.copyOf(message.bids()),
-                message.totalAskNum(),
-                message.totalBidNum()
+                message.stockCode()
         );
     }
 }
