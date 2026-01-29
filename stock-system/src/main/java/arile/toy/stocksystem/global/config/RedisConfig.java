e018f82de0f484b3f3e8497e8893ed23e588a071
arile <dltmddnr0208@gmail.com>
2026-01-29 18:11:20 +0900

    #14 - RedisConfig: template 추가
    Message를 위한 redis template 추가함.

--- stock-system/src/main/java/arile/toy/stocksystem/global/config/RedisConfig.java
+++ stock-system/src/main/java/arile/toy/stocksystem/global/config/RedisConfig.java
@@ -1,8 +1,15 @@
 package arile.toy.stocksystem.global.config;
 
+import arile.toy.stocksystem.bffserver.external.stock.message.BffServerBidAskPriceTickMessage;
+import arile.toy.stocksystem.bffserver.external.stock.message.BffServerStockSummaryTickMessage;
+import arile.toy.stocksystem.bffserver.external.stock.message.BffServerTradePriceTickMessage;
 import arile.toy.stocksystem.stockserver.external.stock.event.BidAskPriceTickEvent;
 import arile.toy.stocksystem.stockserver.external.stock.event.StockSummaryTickEvent;
 import arile.toy.stocksystem.stockserver.external.stock.event.TradePriceTickEvent;
+import arile.toy.stocksystem.stockserver.external.stock.message.BidAskPriceTickMessage;
+import arile.toy.stocksystem.stockserver.external.stock.message.StockSummaryTickMessage;
+import arile.toy.stocksystem.stockserver.external.stock.message.TradePriceTickMessage;
+import arile.toy.stocksystem.stockserver.trading.event.*;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -54,6 +61,72 @@ public class RedisConfig {
         template.setConnectionFactory(redisConnectionFactory);
         template.setKeySerializer(new StringRedisSerializer());
         template.setValueSerializer(new JacksonJsonRedisSerializer<>(BidAskPriceTickEvent.class));
+        return template;
+    }
+
+    @Bean
+    public RedisTemplate<String, TradePriceTickMessage> tradePriceTickMessageRedisTemplate(
+            RedisConnectionFactory redisConnectionFactory) {
+        var template = new RedisTemplate<String, TradePriceTickMessage>();
+        template.setConnectionFactory(redisConnectionFactory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new JacksonJsonRedisSerializer<>(TradePriceTickMessage.class));
+        return template;
+    }
+
+    @Bean
+    public RedisTemplate<String, BidAskPriceTickMessage> bidAskPriceTickMessageRedisTemplate(
+            RedisConnectionFactory redisConnectionFactory) {
+        var template = new RedisTemplate<String, BidAskPriceTickMessage>();
+        template.setConnectionFactory(redisConnectionFactory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new JacksonJsonRedisSerializer<>(BidAskPriceTickMessage.class));
+        return template;
+    }
+
+    @Bean
+    public RedisTemplate<String, StockSummaryTickMessage> stockSummaryTickMessageRedisTemplate(
+            RedisConnectionFactory redisConnectionFactory
+    ) {
+        var template = new RedisTemplate<String, StockSummaryTickMessage>();
+        template.setConnectionFactory(redisConnectionFactory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setHashValueSerializer(new JacksonJsonRedisSerializer<>(StockSummaryTickMessage.class));
+        template.afterPropertiesSet();
+        return template;
+    }
+
+    @Bean
+    public RedisTemplate<String, BffServerTradePriceTickMessage> bffServerTradePriceTickMessageRedisTemplate(
+            RedisConnectionFactory redisConnectionFactory) {
+        var template = new RedisTemplate<String, BffServerTradePriceTickMessage>();
+        template.setConnectionFactory(redisConnectionFactory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new JacksonJsonRedisSerializer<>(BffServerTradePriceTickMessage.class));
+        return template;
+    }
+
+    @Bean
+    public RedisTemplate<String, BffServerBidAskPriceTickMessage> bffServerBidAskPriceTickMessageRedisTemplate(
+            RedisConnectionFactory redisConnectionFactory) {
+        var template = new RedisTemplate<String, BffServerBidAskPriceTickMessage>();
+        template.setConnectionFactory(redisConnectionFactory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new JacksonJsonRedisSerializer<>(BffServerBidAskPriceTickMessage.class));
+        return template;
+    }
+
+    @Bean
+    public RedisTemplate<String, BffServerStockSummaryTickMessage> bffServerStockSummaryTickMessageRedisTemplate(
+            RedisConnectionFactory redisConnectionFactory
+    ) {
+        var template = new RedisTemplate<String, BffServerStockSummaryTickMessage>();
+        template.setConnectionFactory(redisConnectionFactory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setHashValueSerializer(new JacksonJsonRedisSerializer<>(BffServerStockSummaryTickMessage.class));
+        template.afterPropertiesSet();
         return template;
     }
 }
