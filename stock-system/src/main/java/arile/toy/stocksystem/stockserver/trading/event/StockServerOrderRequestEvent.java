03ac21b6ae6f622e57ccfb051be5f266fca01251
arile <dltmddnr0208@gmail.com>
2026-01-31 12:37:31 +0900

    #15 - (stock server) Consumer: Redis Stream 주문 요청 consumer 추가
    consumer group 기반 Redis Stream 메시지 소비 구현함.
    shard index를 이용하여 스트림을 분산 처리 하였으며,  주문 생성 이벤트 처리 후 ACK 처리함.
    추후 재처리 과정 구현 예정

---
+++ stock-system/src/main/java/arile/toy/stocksystem/stockserver/trading/event/StockServerOrderRequestEvent.java
@@ -0,0 +1,15 @@
+package arile.toy.stocksystem.stockserver.trading.event;
+
+import arile.toy.stocksystem.stockserver.trading.dto.order.OrderType;
+
+public record StockServerOrderRequestEvent(
+        String username,
+        String stockCode,
+        OrderType orderType,
+        Integer orderPrice,
+        Integer orderQuantity
+) {
+    public static StockServerOrderRequestEvent of(String username, String stockCode, OrderType orderType, Integer orderPrice, Integer orderQuantity) {
+        return new StockServerOrderRequestEvent(username, stockCode, orderType, orderPrice, orderQuantity);
+    }
+}
