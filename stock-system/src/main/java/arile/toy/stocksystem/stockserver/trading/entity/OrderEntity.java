aed01ad207b6a280792b99c7baadd3c4f8efe95b
arile <dltmddnr0208@gmail.com>
2026-01-31 13:04:54 +0900

    #15 - (stock server) OrderRepository: 주문 정보 db 저장
    주문 정보를 db에 저장하기 위해 repository를 생성함.
    모든 내부 로직(체결, 주문, 취소 등)은  db의 데이터를 기준으로 구현될 것임.

---
+++ stock-system/src/main/java/arile/toy/stocksystem/stockserver/trading/entity/OrderEntity.java
@@ -0,0 +1,63 @@
+package arile.toy.stocksystem.stockserver.trading.entity;
+
+import arile.toy.stocksystem.stockserver.trading.dto.order.OrderStatus;
+import arile.toy.stocksystem.stockserver.trading.dto.order.OrderType;
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.Instant;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+@Entity
+@Table(name = "orders")
+public class OrderEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long orderId;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String stockCode;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private OrderType orderType;
+
+    @Column(nullable = false)
+    private Integer orderPrice;
+
+    @Column(nullable = false)
+    private Integer orderQuantity;
+
+    @Column(nullable = false)
+    private Integer remainingQuantity;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private OrderStatus orderStatus;
+
+    @Column(nullable = false)
+    private Instant orderTime;
+
+    public static OrderEntity of(String username, String stockCode, OrderType orderType,
+                                 Integer orderPrice, Integer orderQuantity,
+                                 OrderStatus orderStatus, Integer remainingQuantity) {
+        var orderEntity = new OrderEntity();
+        orderEntity.setUsername(username);
+        orderEntity.setStockCode(stockCode);
+        orderEntity.setOrderType(orderType);
+        orderEntity.setOrderPrice(orderPrice);
+        orderEntity.setOrderQuantity(orderQuantity);
+        orderEntity.setOrderStatus(orderStatus);
+        orderEntity.setRemainingQuantity(remainingQuantity);
+        orderEntity.setOrderTime(Instant.now());
+        return orderEntity;
+    }
+}
