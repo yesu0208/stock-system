e22dd224e45cc29ff6c3a129340c816b20a7c84a
arile <dltmddnr0208@gmail.com>
2026-02-01 11:44:00 +0900

    #17 - (stock server) RedisConfig: template 정의
    redis publisher에서 사용할 redis template를 정의함.

--- stock-system/src/main/java/arile/toy/stocksystem/global/config/RedisConfig.java
+++ stock-system/src/main/java/arile/toy/stocksystem/global/config/RedisConfig.java
@@ -11,6 +11,7 @@ import arile.toy.stocksystem.stockserver.external.stock.message.StockSummaryTick
 import arile.toy.stocksystem.stockserver.external.stock.message.TradePriceTickMessage;
 import arile.toy.stocksystem.stockserver.trading.event.*;
 import arile.toy.stocksystem.stockserver.useraccount.dto.StockServerAccountMessage;
+import arile.toy.stocksystem.stockserver.useraccount.event.AccountUpdateEvent;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -168,6 +169,16 @@ public class RedisConfig {
         template.setHashKeySerializer(new StringRedisSerializer());
         template.setHashValueSerializer(new JacksonJsonRedisSerializer<>(StockServerAccountMessage.class));
         template.afterPropertiesSet();
+        return template;
+    }
+
+    @Bean
+    public RedisTemplate<String, AccountUpdateEvent> accountUpdateEventRedisTemplate(
+            RedisConnectionFactory redisConnectionFactory) {
+        var template = new RedisTemplate<String, AccountUpdateEvent>();
+        template.setConnectionFactory(redisConnectionFactory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new JacksonJsonRedisSerializer<>(AccountUpdateEvent.class));
         return template;
     }
 }
