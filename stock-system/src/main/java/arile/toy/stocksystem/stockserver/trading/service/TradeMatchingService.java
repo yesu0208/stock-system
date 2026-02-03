ef6bbe0b5b51f5df374b9c3d3c9e6ea8b70d685d
arile <dltmddnr0208@gmail.com>
2026-02-03 23:11:20 +0900

    #25 - (stock server) TradeMatchingService: 체결 후처리 로직 추가
    체결 결과를 Redis의 계좌 정보에 반영함.
    또한, 체결 후 주문의 남은 수량 처리 및 재큐잉 로직을 추가함.
    체결 완료 후, 체결 정보를 담은 event를 bff server로 전송함.

--- stock-system/src/main/java/arile/toy/stocksystem/stockserver/trading/service/TradeMatchingService.java
+++ stock-system/src/main/java/arile/toy/stocksystem/stockserver/trading/service/TradeMatchingService.java
@@ -2,10 +2,18 @@ package arile.toy.stocksystem.stockserver.trading.service;
 
 import arile.toy.stocksystem.stockserver.external.stock.message.TradePriceTickMessage;
 import arile.toy.stocksystem.stockserver.lock.StockLockRegistry;
+import arile.toy.stocksystem.stockserver.trading.dto.order.OrderDto;
 import arile.toy.stocksystem.stockserver.trading.dto.order.OrderQueueRegistry;
+import arile.toy.stocksystem.stockserver.trading.dto.order.OrderStatus;
+import arile.toy.stocksystem.stockserver.trading.dto.order.StockServerOrderResponseMessage;
+import arile.toy.stocksystem.stockserver.trading.event.TradeResponseEvent;
+import arile.toy.stocksystem.stockserver.trading.event.publisher.RedisOrderResponseEventPublisher;
+import arile.toy.stocksystem.stockserver.trading.event.publisher.RedisTradeResponseEventPublisher;
+import arile.toy.stocksystem.stockserver.trading.event.publisher.TradeResponseEventPublisher;
+import arile.toy.stocksystem.stockserver.trading.repository.StockServerOrderResponseRepository;
+import arile.toy.stocksystem.stockserver.trading.repository.TradeCommand;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.locks.ReentrantLock;
@@ -18,6 +26,9 @@ public class TradeMatchingService {
     private final StockLockRegistry stockLockRegistry;
     private final OrderQueueRegistry orderQueueRegistry;
     private final TradeExecutionService tradeExecutionService;
+    private final TradeResponseEventPublisher tradeResponseEventPublisher;
+    private final StockServerOrderResponseRepository stockServerOrderResponseRepository;
+    private final TradeCommand tradeCommand;
 
     public void getExternalTickMessageAndTrade(TradePriceTickMessage tradePriceTickMessage) {
         ReentrantLock lock = stockLockRegistry.lock(tradePriceTickMessage.stockCode());
@@ -48,7 +59,69 @@ public class TradeMatchingService {
 
                 int executable = Math.min(leftQuantity, sellOrderDto.remainingQuantity());
 
-                tradeExecutionService.executeSellTrade(sellOrderDto, tradePrice, executable);
+                var tradeResult = tradeExecutionService.executeSellTrade(sellOrderDto, tradePrice, executable);
+
+                if (tradeResult != null) {
+
+                    int remainingQuantity = sellOrderDto.remainingQuantity() - executable;
+
+                    if (remainingQuantity > 0) {
+                        var reEnqueuedOrderDto = new OrderDto(sellOrderDto.orderId(),
+                                sellOrderDto.username(),
+                                sellOrderDto.stockCode(),
+                                sellOrderDto.orderType(),
+                                sellOrderDto.orderPrice(),
+                                sellOrderDto.orderQuantity(),
+                                remainingQuantity,
+                                OrderStatus.PARTIAL,
+                                sellOrderDto.orderTime());
+                        orderQueueRegistry.orderEnqueue(reEnqueuedOrderDto);
+                    }
+
+                    if (remainingQuantity == 0) {
+                        stockServerOrderResponseRepository.delete(sellOrderDto.username(), sellOrderDto.orderId());
+                    } else {
+                        stockServerOrderResponseRepository.update(tradeResult.tradeEntity().getUsername(),
+                                tradeResult.tradeEntity().getOrderId(),
+                                StockServerOrderResponseMessage.of(
+                                        sellOrderDto.orderId(),
+                                        sellOrderDto.username(),
+                                        sellOrderDto.stockCode(),
+                                        sellOrderDto.orderType(),
+                                        sellOrderDto.orderPrice(),
+                                        sellOrderDto.orderQuantity(),
+                                        remainingQuantity,
+                                        sellOrderDto.orderTime()
+                                ));
+                    }
+
+                    long tradeAmount = (long) sellOrderDto.orderPrice() * tradeResult.tradeEntity().getTradeQuantity();
+                    long differenceAmount = (long) (tradeResult.tradeEntity().getTradePrice() - sellOrderDto.orderPrice()) * tradeResult.tradeEntity().getTradeQuantity();
+
+                    long buyPrice = 0;
+                    if (tradeResult.totalQuantity() != 0) {
+                        buyPrice = tradeResult.totalAmount() / tradeResult.totalQuantity();
+                    }
+
+                    boolean redisOk = tradeCommand.applySellTrade(
+                            tradeResult.tradeEntity().getUsername(),
+                            tradeResult.tradeEntity().getStockCode(),
+                            tradeResult.totalQuantity(),
+                            buyPrice,
+                            tradeAmount,
+                            differenceAmount
+                    );
+
+                    if (!redisOk) {
+                        log.error("Redis sell command failed.");
+                    }
+
+                    tradeResponseEventPublisher.publish(TradeResponseEvent.fromEntity(tradeResult.tradeEntity()));
+                    leftQuantity -= tradeResult.tradeEntity().getTradeQuantity();
+                } else {
+                    log.debug("skip canceled order.");
+                }
+
             }
         } else if (tradingType.equals("5")) { // SELL -> 모의 투자는 BUY
             while (leftQuantity > 0) {
@@ -62,7 +135,61 @@ public class TradeMatchingService {
 
                 int executable = Math.min(leftQuantity, buyOrderDto.remainingQuantity());
 
-                tradeExecutionService.executeBuyTrade(buyOrderDto, tradePrice, executable);
+                var tradeResult = tradeExecutionService.executeBuyTrade(buyOrderDto, tradePrice, executable);
+
+                if (tradeResult != null) {
+
+                    int remainingQuantity = buyOrderDto.remainingQuantity() - executable;
+
+                    if (remainingQuantity > 0) {
+                        var reEnqueuedOrderDto = new OrderDto(buyOrderDto.orderId(),
+                                buyOrderDto.username(),
+                                buyOrderDto.stockCode(),
+                                buyOrderDto.orderType(),
+                                buyOrderDto.orderPrice(),
+                                buyOrderDto.orderQuantity(),
+                                remainingQuantity,
+                                OrderStatus.PARTIAL,
+                                buyOrderDto.orderTime());
+                        orderQueueRegistry.orderEnqueue(reEnqueuedOrderDto);
+                    }
+
+                    if (remainingQuantity == 0) {
+                        stockServerOrderResponseRepository.delete(buyOrderDto.username(), buyOrderDto.orderId());
+                    } else {
+                        stockServerOrderResponseRepository.update(tradeResult.tradeEntity().getUsername(),
+                                tradeResult.tradeEntity().getOrderId(),
+                                StockServerOrderResponseMessage.of(
+                                        buyOrderDto.orderId(),
+                                        buyOrderDto.username(),
+                                        buyOrderDto.stockCode(),
+                                        buyOrderDto.orderType(),
+                                        buyOrderDto.orderPrice(),
+                                        buyOrderDto.orderQuantity(),
+                                        remainingQuantity,
+                                        buyOrderDto.orderTime()
+                                ));
+                    }
+
+                    long tradeAmount = (long) buyOrderDto.orderPrice() * tradeResult.tradeEntity().getTradeQuantity();
+                    long differenceAmount = (long) (buyOrderDto.orderPrice() - tradeResult.tradeEntity().getTradePrice()) * tradeResult.tradeEntity().getTradeQuantity();
+
+                    boolean redisOk = tradeCommand.applySellTrade(
+                            tradeResult.tradeEntity().getUsername(),
+                            tradeResult.tradeEntity().getStockCode(),
+                            tradeResult.totalQuantity(),
+                            tradeResult.totalAmount()/tradeResult.totalQuantity(),
+                            tradeAmount,
+                            differenceAmount
+                    );
+
+                    if (!redisOk) {
+                        log.error("Redis buy command failed.");
+                    }
+
+                    tradeResponseEventPublisher.publish(TradeResponseEvent.fromEntity(tradeResult.tradeEntity()));
+                    leftQuantity -= tradeResult.tradeEntity().getTradeQuantity();
+                }
 
             }
         }
