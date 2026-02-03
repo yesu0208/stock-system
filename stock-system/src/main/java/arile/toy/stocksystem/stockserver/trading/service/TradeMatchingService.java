bc1e06ec245d69eee899411f20e19fcdb42e024e
arile <dltmddnr0208@gmail.com>
2026-02-03 21:14:16 +0900

    #25 - (stock server) TradeMatchingService: 체결 가능 조건 확인 로직 작성
    tick message의 정보를 바탕으로 종목 별 PQ의 주문 정보를 확인하고,
    조건에 부합하면 TradeExecutionService로 이동하여 실제 체결 과정을 거치게 됨.
    PQ에서 peek를 하지 않고 바로 poll을 한 후 조건을 확인하는 방식을 택했는데,
    peek를 하여 조건 확인 후 poll을 하면,
    peek과 poll의 대상이 달라질 수 있음을 인지하였음.
    (예를 들어, peek 이후 해당 주문이 취소되어 PQ에서 제거되면 poll의 대상이 peek의 대상과 달라질 수 있음)
    외부 tick message(실거래)가 매도일 경우, 실제 모의 투자 서비스는 매수 체결이 이루어짐.

--- stock-system/src/main/java/arile/toy/stocksystem/stockserver/trading/service/TradeMatchingService.java
+++ stock-system/src/main/java/arile/toy/stocksystem/stockserver/trading/service/TradeMatchingService.java
@@ -2,8 +2,10 @@ package arile.toy.stocksystem.stockserver.trading.service;
 
 import arile.toy.stocksystem.stockserver.external.stock.message.TradePriceTickMessage;
 import arile.toy.stocksystem.stockserver.lock.StockLockRegistry;
+import arile.toy.stocksystem.stockserver.trading.dto.order.OrderQueueRegistry;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.locks.ReentrantLock;
@@ -14,6 +16,8 @@ import java.util.concurrent.locks.ReentrantLock;
 public class TradeMatchingService {
 
     private final StockLockRegistry stockLockRegistry;
+    private final OrderQueueRegistry orderQueueRegistry;
+    private final TradeExecutionService tradeExecutionService;
 
     public void getExternalTickMessageAndTrade(TradePriceTickMessage tradePriceTickMessage) {
         ReentrantLock lock = stockLockRegistry.lock(tradePriceTickMessage.stockCode());
@@ -27,6 +31,40 @@ public class TradeMatchingService {
     }
 
     private void matchAndExecuteWithinLock(TradePriceTickMessage tradePriceTickMessage) {
+        String stockCode = tradePriceTickMessage.stockCode();
+        int tradePrice = tradePriceTickMessage.curPrice();
+        int leftQuantity = tradePriceTickMessage.tradingVolumeTick();
+        String tradingType = tradePriceTickMessage.tradingType(); // 1:BUY, 5:SELL
+
+        if (tradingType.equals("1")) { // BUY -> 모의 투자는 SELL
+            while (leftQuantity > 0) {
+                var sellOrderDto = orderQueueRegistry.pollSell(stockCode);
+                if (sellOrderDto == null) return;
+
+                if (sellOrderDto.orderPrice() > tradePrice) {
+                    orderQueueRegistry.orderEnqueue(sellOrderDto);
+                    return;
+                }
+
+                int executable = Math.min(leftQuantity, sellOrderDto.remainingQuantity());
+
+                tradeExecutionService.executeSellTrade(sellOrderDto, tradePrice, executable);
+            }
+        } else if (tradingType.equals("5")) { // SELL -> 모의 투자는 BUY
+            while (leftQuantity > 0) {
+                var buyOrderDto = orderQueueRegistry.pollBuy(stockCode);
+                if (buyOrderDto == null) return;
+
+                if (buyOrderDto.orderPrice() < tradePrice) {
+                    orderQueueRegistry.orderEnqueue(buyOrderDto);
+                    return;
+                }
+
+                int executable = Math.min(leftQuantity, buyOrderDto.remainingQuantity());
+
+                tradeExecutionService.executeBuyTrade(buyOrderDto, tradePrice, executable);
+
+            }
+        }
     }
 }
-
