3cf7cf220a4d7256c877edc0fc0c3972c06b278d
arile <dltmddnr0208@gmail.com>
2026-02-03 21:41:32 +0900

    #25 - (stock server) Order/UserAccountRepository: 동시성 제어 조회 추가
    체결 과정에서 DB의 주문 정보와 계좌 정보를 변경해야 하는데,
    동시에 다른 로직에서 이에 대해 읽거나 쓸 경우 문제 발생할 수 있음. (특히, 주문 삭제 로직 및 다른 종목 체결 과정에서 계좌 접근)
    따라서, PESSIMISTIC_WRITE 잠금 적용 조회를 만들어 동시 트랜잭션에서 해당 레코드의 읽기/쓰기를 모두 차단함.
    보유 종목 DB(UserStockRepository)는 동시에 접근되는 경우가 없으므로(같은 종목의 체결은 이미 lock으로 동시에 일어나지 않음) 이를 적용하지 않음.

--- stock-system/src/main/java/arile/toy/stocksystem/stockserver/trading/repository/OrderRepository.java
+++ stock-system/src/main/java/arile/toy/stocksystem/stockserver/trading/repository/OrderRepository.java
@@ -1,9 +1,18 @@
 package arile.toy.stocksystem.stockserver.trading.repository;
 
 import arile.toy.stocksystem.stockserver.trading.entity.OrderEntity;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface OrderRepository extends JpaRepository<OrderEntity, Long> {
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("select o from OrderEntity o where o.orderId = :orderId")
+    Optional<OrderEntity> findByIdForUpdate(@Param("orderId") Long orderId);
 }
