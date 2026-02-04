27b79fbf6a9fb86e1573f6bac1aaca5fba0ba57e
arile <dltmddnr0208@gmail.com>
2026-02-04 22:33:23 +0900

    #27 - (stock server) OrderRequestEvent: fromAutoOrderDto
    자동 주문이 trigger되어 주문 로직이 이루어질 떄,
    자동 주문 정보가 일반 주문 정보로 변환되어야 함.
    따라서, fromAutoOrderDto method 추가함.
    AutoOrderType도 OrderType으로 변환되어야 함.

--- stock-system/src/main/java/arile/toy/stocksystem/stockserver/trading/dto/auto/order/AutoOrderType.java
+++ stock-system/src/main/java/arile/toy/stocksystem/stockserver/trading/dto/auto/order/AutoOrderType.java
@@ -1,6 +1,15 @@
 package arile.toy.stocksystem.stockserver.trading.dto.auto.order;
 
+import arile.toy.stocksystem.stockserver.trading.dto.order.OrderType;
+
 public enum AutoOrderType {
     BUY,
     SELL;
+
+    public OrderType toOrderType() {
+        return switch (this) {
+            case BUY  -> OrderType.BUY;
+            case SELL -> OrderType.SELL;
+        };
+    }
 }
