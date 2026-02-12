20163f1d762d1a4daf13a9ca364f1d1db31e5248
arile <dltmddnr0208@gmail.com>
2026-02-12 21:10:28 +0900

    #29 - AccountBalanceCommand: reserve/refund stock
    해당 class를 통해 매도 로직을 진행할 예정임.
    Redis의 availableQuantity(가능 수량)을 제어하면서 매도/매도 취소 로직이 진행됨.

--- stock-system/src/main/java/arile/toy/stocksystem/stockserver/useraccount/repository/AccountBalanceCommand.java
+++ stock-system/src/main/java/arile/toy/stocksystem/stockserver/useraccount/repository/AccountBalanceCommand.java
@@ -3,5 +3,7 @@ package arile.toy.stocksystem.stockserver.useraccount.repository;
 public interface AccountBalanceCommand {
     boolean reserveCash(String username, long amount);
     boolean refundReservedCash(String username, long amount);
+    boolean reserveStock(String username, String stockCode, int quantity);
+    boolean refundReservedStock(String username, String stockCode, int quantity);
 }
 
