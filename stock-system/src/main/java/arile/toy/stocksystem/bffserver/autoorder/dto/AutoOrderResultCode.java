043fae018efae8b2f2c3e28195eeccb3bf31b5a7
arile <dltmddnr0208@gmail.com>
2026-02-12 21:26:21 +0900

    #29 - Error/StatusCode: enum INSUFFICIENT_STOCK 추가
    Redis의 계좌 정보에 주식의 availableQuantity(가능 수량)을 새롭게 추가하였고,
    (자동)매도/매도 취소 로직에서 이를 사용함.

--- stock-system/src/main/java/arile/toy/stocksystem/bffserver/autoorder/dto/AutoOrderResultCode.java
+++ stock-system/src/main/java/arile/toy/stocksystem/bffserver/autoorder/dto/AutoOrderResultCode.java
@@ -2,6 +2,7 @@ package arile.toy.stocksystem.bffserver.autoorder.dto;
 
 public enum AutoOrderResultCode {
     INSUFFICIENT_BALANCE("[자동 주문 거절] 잔액이 부족하여 자동 주문에 실패했습니다."),
+    INSUFFICIENT_STOCK("[자동 주문 거절] 보유 주식이 부족하여 자동 주문에 실패했습니다."),
     INTERNAL_ERROR("[자동 주문 거절] 일시적인 오류가 발생했습니다. 잠시 후 다시 시도해주세요."),
     TRIGGERED("주문 triggered.");
     private final String userMessage;
