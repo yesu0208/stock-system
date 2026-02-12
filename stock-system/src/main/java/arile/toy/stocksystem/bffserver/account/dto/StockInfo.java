3cea26fe8bb96529b562664cf45a7a739caea432
arile <dltmddnr0208@gmail.com>
2026-02-12 20:16:01 +0900

    #29 - StockInfo: availableQuantity 필드 추가
    redis의 사용자 계좌 정보에 보유한 주식에 대한 정보를 담고 있는데,
    매도 주문을 할 경우 주식의 가능 수량에서 차감하는 방식을 적용할 것임. (매도 주문 취소 시 가능 수량을 복원함.)
    따라서, availableQuantity 필드를 추가함.

--- stock-system/src/main/java/arile/toy/stocksystem/bffserver/account/dto/StockInfo.java
+++ stock-system/src/main/java/arile/toy/stocksystem/bffserver/account/dto/StockInfo.java
@@ -2,6 +2,8 @@ package arile.toy.stocksystem.bffserver.account.dto;
 
 public record StockInfo(
         Integer quantity,
+        Integer availableQuantity,
         Integer buyPrice
+
 ) {
 }
--- stock-system/src/main/java/arile/toy/stocksystem/stockserver/useraccount/dto/StockInfo.java
+++ stock-system/src/main/java/arile/toy/stocksystem/stockserver/useraccount/dto/StockInfo.java
@@ -2,10 +2,11 @@ package arile.toy.stocksystem.stockserver.useraccount.dto;
 
 public record StockInfo(
         Integer quantity,
+        Integer availableQuantity,
         Integer buyPrice
 )
 {
-    public static StockInfo of(Integer quantity, Integer buyPrice) {
-        return new  StockInfo(quantity, buyPrice);
+    public static StockInfo of(Integer quantity, Integer availableQuantity, Integer buyPrice) {
+        return new StockInfo(quantity, availableQuantity, buyPrice);
     }
 }
