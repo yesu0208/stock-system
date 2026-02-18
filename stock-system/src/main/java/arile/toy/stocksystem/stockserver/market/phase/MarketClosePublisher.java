52c27be9998dfc202ae5069b07a1bd9115de5862
arile <dltmddnr0208@gmail.com>
2026-02-18 15:24:44 +0900

    #36 - (stock server) MarketClosePublisher: Redis Pub/Sub 기반 장 종료 event 발행 기능 구현
    Redis 채널(market:close)에 장 종료 event 발행 로직을 구현함.

---
+++ stock-system/src/main/java/arile/toy/stocksystem/stockserver/market/phase/MarketClosePublisher.java
@@ -0,0 +1,23 @@
+package arile.toy.stocksystem.stockserver.market.phase;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class MarketClosePublisher {
+
+    private final StringRedisTemplate redisTemplate;
+    private static final String MARKET_CLOSE_CHANNEL = "market:close";
+
+    public void publishMarketClose() {
+
+        String message = "MARKET_CLOSED";
+
+        redisTemplate.convertAndSend(MARKET_CLOSE_CHANNEL, message);
+        log.info("[RedisPublisher] Published market close event to channel {}", MARKET_CLOSE_CHANNEL);
+    }
+}
