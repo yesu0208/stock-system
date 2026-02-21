2ee81537a2509ade81747539a043e5c9b3f396fb
arile <dltmddnr0208@gmail.com>
2026-02-21 11:19:26 +0900

    #42 - vite.config.ts
    define.global을 window로 지정하여 일부 라이브러리 호환성 처리함.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    global: 'window', // global을 window로 지정
+  },
 })
