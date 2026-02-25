fc6775305aab1c52b0c07e7ed02d2a67feb2567e
arile <dltmddnr0208@gmail.com>
2026-02-25 19:29:39 +0900

    #51 - (BE) application.yaml: 환경 변수로 setting
    하드 코딩 대신 환경변수로 setting함.

--- backend/src/main/java/arile/toy/stocksystem/bffserver/security/config/SecurityConfig.java
+++ backend/src/main/java/arile/toy/stocksystem/bffserver/security/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package arile.toy.stocksystem.bffserver.security.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -24,10 +25,13 @@ public class SecurityConfig {
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
     private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
 
+    @Value("${frontend.url}")
+    private String frontendUrl;
+
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173", "http://127.0.0.1:5173"));
+        configuration.setAllowedOrigins(List.of(frontendUrl));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
