1bc9be7fc552f003c00ebd6c92762db36d78a3c7
arile <dltmddnr0208@gmail.com>
2026-02-25 23:44:59 +0900

    #54 - ECS module: main.tf
    - IAM Role 추가
    - CloudWatch Log Group 추가
    - Security Group 추가
    (Ingress는 임시로 세팅)

--- infrastructure/modules/ecs/main.tf
+++ infrastructure/modules/ecs/main.tf
@@ -2,6 +2,53 @@ resource "aws_ecs_cluster" "this" {
   name = "${var.environment}-ecs"
 }
 
+# IAM Role
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "${var.environment}-ecs-task-execution-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ecs-tasks.amazonaws.com"
+      }
+    }]
+  })
+}
+
+# Policy
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+# CloudWatch Log Group
+resource "aws_cloudwatch_log_group" "ecs" {
+  name = "/ecs/${var.environment}"
+}
+
+# Security Group
+resource "aws_security_group" "ecs" {
+  name   = "${var.environment}-ecs-sg"
+  vpc_id = var.vpc_id
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # 일단 임시 (나중에 ALB SG로 제한)
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 output "service_arn" {
   value = aws_ecs_cluster.this.arn
 }
