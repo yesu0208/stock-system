8b2c59eeaab66e0397449d7f99a67439d46bf863
arile <dltmddnr0208@gmail.com>
2026-02-25 23:01:49 +0900

    #54 - VPC module: VPC 및 subnet, IGW, NAT Gateway, route table 구성
    - VPC 생성
    - Public/Private Subnet 구성
    - Internet Gateway 연결
    - NAT Gateway 설정
    - Public/Private Route Table 및 Association 생성

--- infrastructure/modules/vpc/main.tf
+++ infrastructure/modules/vpc/main.tf
@@ -32,3 +32,64 @@ resource "aws_subnet" "private" {
     Name = "${var.environment}-private-${each.key}"
   }
 }
+
+# NAT Elastic IP
+resource "aws_eip" "nat" {
+  domain = "vpc"
+}
+
+# NAT GateWay
+resource "aws_nat_gateway" "this" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = values(aws_subnet.public)[0].id
+
+  tags = {
+    Name = "${var.environment}-nat"
+  }
+
+  depends_on = [aws_internet_gateway.igw]
+}
+
+# Public Route Table
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "${var.environment}-public-rt"
+  }
+}
+
+# Private Route Table
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.this.id
+  }
+
+  tags = {
+    Name = "${var.environment}-private-rt"
+  }
+}
+
+# Route Table Association(public)
+resource "aws_route_table_association" "public" {
+  for_each = aws_subnet.public
+
+  subnet_id      = each.value.id
+  route_table_id = aws_route_table.public.id
+}
+
+# Route Table Association(private)
+resource "aws_route_table_association" "private" {
+  for_each = aws_subnet.private
+
+  subnet_id      = each.value.id
+  route_table_id = aws_route_table.private.id
+}
