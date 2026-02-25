2e3641343e048f79d86cc9b58e435a0f6ff31a31
arile <dltmddnr0208@gmail.com>
2026-02-25 20:16:55 +0900

    #51 - (FE) VITE 환경 변수 사용
    CI/CD pipeline에서는 npm run build할 때 환경 변수로 주입할 예정임.

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -7,7 +7,7 @@ interface CustomAxiosRequestConfig extends AxiosRequestConfig {
 }
 
 const instance = axios.create({
-    baseURL: 'http://localhost:8080/api/v1',
+    baseURL: import.meta.env.VITE_API_BASE_URL,
     headers: { 'Content-Type': 'application/json' },
     withCredentials: true,
 })
