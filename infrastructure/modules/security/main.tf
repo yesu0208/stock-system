fac9b008b8b5850979e3fb14c2908a12eb8ede23
arile <dltmddnr0208@gmail.com>
2026-02-26 12:46:32 +0900

    #54 - ALB Security Group 수정
    Route53 + ACM 사용할 것이므로,
    port를 수정함. (http -> https)

--- infrastructure/modules/security/main.tf
+++ infrastructure/modules/security/main.tf
@@ -60,8 +60,8 @@ resource "aws_security_group" "alb" {
   vpc_id = var.vpc_id
 
   ingress {
-    from_port   = 80
-    to_port     = 80
+    from_port   = 443
+    to_port     = 443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
