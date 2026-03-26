44c48e8ec20dab755f5cd8768b009744acb0c5fb
arile <dltmddnr0208@gmail.com>
2026-03-26 12:08:41 +0900

    종목 추가

--- frontend/src/constants/stocks.ts
+++ frontend/src/constants/stocks.ts
@@ -1,8 +1,14 @@
 export const STOCKS = [
     { code: '005930', name: '삼성전자' },
     { code: '000660', name: 'SK하이닉스' },
+    { code: '005935', name: '삼성전자우' },
+    { code: '005380', name: '현대차' },
+    { code: '373220', name: 'LG에너지솔루션' },
+    { code: '000250', name: '삼천당제약' },
+    { code: '196170', name: '알테오젠' },
     { code: '086520', name: '에코프로' },
     { code: '247540', name: '에코프로비엠' },
+    { code: '277810', name: '레인보우로보틱스' },
 ]
 
 export const stockNameMap = STOCKS.reduce<Record<string, string>>(
