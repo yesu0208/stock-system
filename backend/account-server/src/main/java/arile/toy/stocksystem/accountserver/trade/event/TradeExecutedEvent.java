8e610573135c6e4a01a95654a23fc15c88926a6a
arile <dltmddnr0208@gmail.com>
2026-08-29 23:09:04 +0900

    #77 - account-server에 체결 이벤트 반영 로직 및 consumer 추가
    - TradeType, TradeExecutedEvent(account-server 버전) 추가
    - buyTradeScript, sellTradeScript를 AccountLuaConfig로 이관
    - TradeCommand(interface), RedisTradeCommand(구현체) 이관

---
+++ backend/account-server/src/main/java/arile/toy/stocksystem/accountserver/trade/event/TradeExecutedEvent.java
@@ -0,0 +1,14 @@
+package arile.toy.stocksystem.accountserver.trade.event;
+
+import arile.toy.stocksystem.accountserver.trade.dto.TradeType;
+
+public record TradeExecutedEvent(
+        Long tradeId,
+        Long orderId,
+        String username,
+        String stockCode,
+        TradeType tradeType,
+        Integer tradePrice,
+        Integer tradeQuantity
+) {
+}
