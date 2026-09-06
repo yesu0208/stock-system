0d1e6a5d46b3458aef336d19383755f5e1a7983e
arile <dltmddnr0208@gmail.com>
2026-09-06 15:03:29 +0900

    #84 - (stock server) TrailingStopResultCode enum 추가
    - 잔고/주식 부족, 내부 에러, TRIGGERED, TRAILING_UPDATE 구분

---
+++ backend/stock-server/src/main/java/arile/toy/stocksystem/stockserver/trailingstop/dto/TrailingStopResultCode.java
@@ -0,0 +1,9 @@
+package arile.toy.stocksystem.stockserver.trailingstop.dto;
+
+public enum TrailingStopResultCode {
+    INSUFFICIENT_BALANCE,
+    INSUFFICIENT_STOCK,
+    INTERNAL_ERROR,
+    TRIGGERED,
+    TRAILING_UPDATED
+}
