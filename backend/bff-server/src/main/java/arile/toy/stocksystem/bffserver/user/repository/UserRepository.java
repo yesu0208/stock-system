8db80714550bc6fc31b18448848be311c9884a06
arile <dltmddnr0208@gmail.com>
2026-09-08 21:07:50 +0900

    #91 - (bff server) UserRepository: 조회 메서드 추가
    - findByUsernameIn 배치 조회 메서드 추가 (프로젝션 인터페이스 사용)

--- backend/bff-server/src/main/java/arile/toy/stocksystem/bffserver/user/repository/UserRepository.java
+++ backend/bff-server/src/main/java/arile/toy/stocksystem/bffserver/user/repository/UserRepository.java
@@ -4,10 +4,20 @@ import arile.toy.stocksystem.bffserver.user.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
     Optional<UserEntity> findByUsername(String username);
     boolean existsByNickname(String nickname);
+
+    List<UserProfileProjection> findByUsernameIn(Collection<String> usernames);
+
+    interface UserProfileProjection {
+        String getUsername();
+        String getNickname();
+        String getProfileImageUrl();
+    }
 }
